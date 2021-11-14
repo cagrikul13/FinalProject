c7b76e7f985694b233d34d719d679e2e39bcc86f
Hakan Kadakal <hakankadakal@eskisehir.edu.tr>
2021-11-14 22:24:31 +0300

    MainPage added, navigation to login and sign in pages are added

---
+++ FinalProject/FinalProject/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using FinalProject.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FinalProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void loginBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+        private async void signUpBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+    }
+}
