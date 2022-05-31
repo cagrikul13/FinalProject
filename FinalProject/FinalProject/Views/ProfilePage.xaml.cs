29dd765496e879035d07c1f3f3a23ecbf6d6dd11
cagrikul13 <cagrikul@hotmail.com>
2022-06-01 02:04:43 +0300

    profile page ve qrcodepage ui fix

--- FinalProject/FinalProject/Views/ProfilePage.xaml.cs
+++ FinalProject/FinalProject/Views/ProfilePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FinalProject.Models;
+using FinalProject.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,25 @@ namespace FinalProject.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+        FirebaseHelper firebase = new FirebaseHelper();
+        HomePageViewModel profilePageVM;
+        List<Activities> profilePageList;
+
         public ProfilePage()
         {
             InitializeComponent();
+            profilePageVM = new HomePageViewModel();
+            BindingContext = profilePageVM;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            profilePageList = await firebase.GetActivities();
+            ppActivityList.BindingContext = profilePageList;
+
         }
     }
+
+
 }
